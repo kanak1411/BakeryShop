0b1ee95695efa12ab084b84f651dc379471c88e4
Kanak Agarwal <kanak1411@gmail.com>
2022-12-29 22:52:57 +0530

    Bakery Website

---
+++ src/pages/shop/product.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import deleteIcon from './deleteIcon.svg'
+
+export const Product = (props) => {
+  const { id, productName, price, productImage } = props.data;
+  const { addToCart, cartItems,updateCartItemCount } = useContext(ShopContext);
+
+  const cartItemCount = cartItems[id];
+
+  return (
+    <div className="product">
+      <img src={productImage} style={{height:'250px',maxWidth:'300px'}}/>
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p> ${price}</p>
+      </div>
+      <div style={{display:'flex',justifyContent:'center'}}>
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+        Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
+      </button>
+      <img src={deleteIcon} alt="icon" className="deleteIcon" onClick={()=>updateCartItemCount(0,id)}/>
+      </div>
+    </div>
+  );
+};
---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Burger",
+    price: 99.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Bread Basket",
+    price: 19.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Jeera Cookie",
+    price: 6.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Fruit Cup Cake",
+    price: 28.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Choclate Pastry",
+    price: 19.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Roll",
+    price: 8.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Choclate Cookie",
+    price: 12.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Strawberry Cupcake",
+    price: 40.0,
+    productImage: product8,
+  },
+];
